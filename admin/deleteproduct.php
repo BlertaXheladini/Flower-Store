a71aeb075bc4e4e0261f85dd663a3eded2d7072b
blerta xheladini <blerta.xheladini03@gmail.com>
2024-01-30 15:55:44 +0100

    added session for role

--- admin/deleteproduct.php
+++ admin/deleteproduct.php
@@ -3,12 +3,10 @@ include '../connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["deleteId"])) {
     $deleteId = $_POST["deleteId"];
-    
-    // Perform the delete operation based on the product ID
+
     $deleteSql = "DELETE FROM products WHERE id = $deleteId";
     $conn->query($deleteSql);
 
-    // Redirect back to the product list page after deletion
     header("Location: products.php  ");
     exit();
 }
